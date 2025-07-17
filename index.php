725b83c12251e9069950c8932bc66d01276b338b
mannuccimattia <mannuccimattia@gmail.com>
2025-07-17 15:45:57 +0200

    Init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php require_once "./utils/html_head.php" ?>
+  <title>Vinyl Collection</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="mt-5">
+      <h1>My collection</h1>
+    </div>
+
+  </div>
+</body>
+
+</html>
